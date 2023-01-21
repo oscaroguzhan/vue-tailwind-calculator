50aab866837d5ff459df37a4bc6162f583fa16f4
oscaroguzhan <72919227+oscaroguzhan@users.noreply.github.com>
2023-01-21 08:09:18 +0100

    initial setup and clean up

---
+++ src/components/Calculator.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="container" class="flex justify-center items-center h-screen bg-slate-200 flex-col">
+    <h1 class="text-5xl text-center my-8">Calculator</h1>
+    <div id="wrapper">
+      <div id="display" class="flex flex-col">
+        <label for="">Månadkostnad</label>
+        <span>2450</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+
+}
+</script>
